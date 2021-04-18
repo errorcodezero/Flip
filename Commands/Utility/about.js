e1394c4a1d07778de400391b41aa4f74633feaa8
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2021-04-18 09:22:47 -0700

    Making an about command

---
+++ Commands/Utility/about.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'about',
+	description: 'Tells some information about the bot',
+	execute(message) {
+		message.reply('Coding Language: Javascript\nFramework: NodeJS\nOwner: Yeetsquad747');
+	},
+};
